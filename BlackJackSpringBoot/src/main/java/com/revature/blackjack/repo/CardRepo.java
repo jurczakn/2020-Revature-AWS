6a4a7c9cab660c3096a32dc2648f3043ae8318f6
jurczakn <nickolas.jurczak@gmail.com>
2020-07-29 11:53:32 -0400

    blackjackboot added

---
+++ BlackJackSpringBoot/src/main/java/com/revature/blackjack/repo/CardRepo.java
@@ -0,0 +1,15 @@
+package com.revature.blackjack.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.blackjack.domain.Card;
+import com.revature.blackjack.domain.CardId;
+
+@Repository
+public interface CardRepo extends JpaRepository<Card, CardId>{
+	
+	public List<Card> findByValue(int value);
+}
